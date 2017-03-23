b907e055ee7860e5fa536f00063459b3730a6728
Daniel Clives <danieljamesclives@hotmail.com>
2017-03-23 11:27:42 +0000

    Add files via upload

---
+++ seantest/bootstrap/test.php
@@ -0,0 +1,18 @@
+
+<? php 
+#start php here 
+
+
+$var = 13;
+
+
+$var2 = "goodbyemars";
+
+$var3 =+ ($var + $var2;
+
+
+echo  '$var';
+
+//end php here
+
+?>
---
+++ seantest/bootstrap/untitled.hmtl.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+
+
+  <body>
+
+    
+
+<?php 
+#start php here 
+
+
+$var = "helloworld";
+$var2 = "goodbyetomars";
+
+
+
+
+echo "At the end of the mission the team <em> waved </em> $var2;"
+
+
+
+#end php here
+?>
+
+
+
+
+    <h1>Hello, world!</h1>
+
+
+
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
